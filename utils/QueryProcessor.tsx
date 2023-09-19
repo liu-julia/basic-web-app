d595dbd36a5551476fa0194c7e6bb9bf433649bc
Julia Liu <julialiu1233@gmail.com>
2023-09-19 16:10:51 -0400

    cubesquare

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -38,5 +38,58 @@ export default function QueryProcessor(query: string): string {
     const z: number = parseInt (maxInt[3]);
     return Math.max(x, y, z).toString();
   }
+
+  const sqcube = query.match(/Which of the following numbers is both a square and a cube: (\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)/);
+  if (sqcube) {
+    const x: number = parseInt (sqcube[1]);
+    const y: number = parseInt (sqcube[2]);
+    const z: number = parseInt (sqcube[3]);
+    
+    const a: number = parseInt (sqcube[4]);
+    const b: number = parseInt (sqcube[5]);
+    const c: number = parseInt (sqcube[6]);
+    const d: number = parseInt (sqcube[7]);
+    
+    if (Number.isInteger(Math.sqrt(a))) {
+      if (Number.isInteger(Math.cbrt(a))) {
+        return a.toString();
+      }
+    }
+
+    if (Number.isInteger(Math.sqrt(b))) {
+      if (Number.isInteger(Math.cbrt(b))) {
+        return b.toString();
+      }
+    }
+
+    if (Number.isInteger(Math.sqrt(c))) {
+      if (Number.isInteger(Math.cbrt(c))) {
+        return c.toString();
+      }
+    }
+
+    if (Number.isInteger(Math.sqrt(d))) {
+      if (Number.isInteger(Math.cbrt(d))) {
+        return d.toString();
+      }
+    }
+    if (Number.isInteger(Math.sqrt(x))) {
+      if (Number.isInteger(Math.cbrt(x))) {
+        return x.toString();
+      }
+    }
+
+    if (Number.isInteger(Math.sqrt(y))) {
+      if (Number.isInteger(Math.cbrt(y))) {
+        return y.toString();
+      }
+    }
+    if (Number.isInteger(Math.sqrt(z))) {
+      if (Number.isInteger(Math.cbrt(z))) {
+        return z.toString();
+      }
+    }
+  }
   return "";
+
 }
